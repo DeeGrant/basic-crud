dc6259eb1cdaadc4d07cb0749f9e5086cf49d174
Dee Grant <WalterGrant@gmail.com>
2021-12-24 11:51:30 -0600

    add dependencies

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
-const express = require('express')
 const cors = require('cors')
+require('dotenv').config()
+const express = require('express')
+const MongoClient = require('mongodb').MongoClient
+
 const app = express()
 
+app.set('view engine', 'ejs')
 app.use(cors())
 app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+let db
+const dbConnectionStr = process.env.DB_STRING,
+    dbName = process.env.DB_NAME
+
+MongoClient.connect(dbConnectionStr)
+    .then(client => {
+        db = client.db(dbName)
+        console.log(`Connected to ${dbName} database.`)
+    })
 
 const PORT = process.env.PORT || 8000
 app.listen(PORT, () =>
