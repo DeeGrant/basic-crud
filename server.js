331ef78d5b9b89d2234461435a373b09ee5eb11a
Dee Grant <WalterGrant@gmail.com>
2021-12-24 14:26:56 -0600

    add form and post

--- server.js
+++ server.js
@@ -13,9 +13,10 @@ app.use(express.json())
 
 let db
 const dbConnectionStr = process.env.DB_STRING,
-    dbName = process.env.DB_NAME
+    dbName = process.env.DB_NAME,
+    TODOS = 'todos'
 
-MongoClient.connect(dbConnectionStr)
+MongoClient.connect(dbConnectionStr, {useUnifiedTopology: true})
     .then(client => {
         db = client.db(dbName)
         console.log(`Connected to ${dbName} database.`)
@@ -23,6 +24,18 @@ MongoClient.connect(dbConnectionStr)
 
 app.get('/', (req, res) => {
     res.render('index.ejs')
+})
+
+app.post('/api/add-todo', (req, res) => {
+    db.collection(TODOS).insertOne({
+        what: req.body.what,
+        priority: req.body.priority
+    })
+    .then(result => {
+        console.log('Todo added')
+        res.redirect('/')
+    })
+    .catch(e => {console.error(e)})
 })
 
 const PORT = process.env.PORT || 8000
